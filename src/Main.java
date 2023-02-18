276789e5b206d41749ade0c86efcafc58ae24ef9
Azat <aelchikushuulu@gmail.com>
2023-02-18 17:34:56 +0600

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1 () {
@@ -64,5 +65,17 @@ public class Main {
         System.out.println(paper);
         paper = paper - 7639;
         System.out.println(paper);
+    }
+
+    public static void task4 () {
+
+        System.out.println("Задача 4");
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
